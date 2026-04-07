861114d4f9fdf16b5be12d9f4d82a16ad242f836
trionoaja <trionoaja@gmail.com>
2026-04-07 19:48:00 +0700

    pertemuan 6

--- application/models/kategori_model.php
+++ application/models/kategori_model.php
@@ -12,4 +12,18 @@ class kategori_model extends CI_Model {
     {
         return $this->db->insert($this->table, $data);
     }
+    public function get_by_id($id)
+    {
+        $this->db->where('id',$id);
+        return $this->db->get('kategori')->row();
+    }
+    public function delete($id)
+    {
+        return $this->db->delete($this->table,['id'=>$id]);
+    }
+    public function update($id, $data)
+    {
+        $this->db->where('id',$id);
+        return $this->db->update($this->table, $data);
+    }
 }
