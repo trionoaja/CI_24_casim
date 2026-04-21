7551dc8584271127caa24be54bc14aa9ce9d0cb7
trionoaja <trionoaja@gmail.com>
2026-04-21 20:01:01 +0700

    pertemuan 8

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -4,7 +4,38 @@
 
 <script src="<?= base_url('assets/vendor/jquery/jquery.min.js');?>"></script>
 <script src="<?= base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js');?>"></script>
+<script src="<?= base_url('assets/vendor/chart.js/Chart.min.js');?>"></script>
 <script src="<?= base_url('assets/js/sb-admin-2.min.js');?>"></script>
 
+<script>
+    var ctx=document.getElementById("chartDashboard");
+    var chart= new Chart(ctx,{
+        type:'bar',
+        data:{
+            labels:['Kategori','Anggota'],
+            datasets:[{
+                label:'Jumlah Data',
+                data:[
+                    <?=$total_kategori;?>,
+                    <?=$total_anggota;?>
+                ],
+                backgroundColor:[
+                    '#4e73df',
+                    '#1cc88a'
+                ]
+            }]
+        },
+        options:{
+            responsive: true,
+            scales:{
+                yAxes:[{
+                    ticks:{
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+    </script>
 </body>
 </html>
