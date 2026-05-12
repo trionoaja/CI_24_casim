3db7c54903216abaca7118a8d15f590f8345eaa9
trionoaja <trionoaja@gmail.com>
2026-05-12 19:59:52 +0700

    pertemuan 11

--- application/controllers/peminjaman.php
+++ application/controllers/peminjaman.php
@@ -54,4 +54,20 @@ class peminjaman extends CI_Controller {
         $this->Peminjaman_model->pengembalian($id);
         redirect('peminjaman');
     }
+    public function cetak_peminjaman()
+    {
+        $bulan= $this->input->get('bulan');
+
+        $this->db->select('peminjaman.*, anggota.nama');
+        $this->db->from('peminjaman');
+        $this->db->join('anggota', 'anggota.id = peminjaman.anggota_id');
+
+        if($bulan){
+            $this->db->where('DATE_FORMAT(tanggal_pinjam, "%Y-%m")=', $bulan);
+        }
+
+        $data['data']= $this->db->get()->result();
+        $data['bulan']=$bulan;
+        $this->load->view('laporan/cetak_pinjam', $data);
+    }
 }
